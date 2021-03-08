28203d843b41d281b553f942079234f451315a9e
webmaster978 <jtjoel11@gmail.com>
2021-03-08 18:13:06 +0200

    evolution

--- example-app/resources/views/welcome.blade.php
+++ example-app/resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master',['title' => 'Home'])
 
-        <title>Joel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@section('content')
+<h1>Hello</h1>
 
 
-    </body>
-</html>
+@endsection
